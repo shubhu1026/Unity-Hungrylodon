b66f91efc11238fe42828b205fec62001f497d36
shubhu1026 <shubhu.p2000@gmail.com>
2023-02-08 00:05:58 +0530

    added powerup probabilities

--- Assets/Scripts/PowerupSpawner.cs
+++ Assets/Scripts/PowerupSpawner.cs
@@ -8,7 +8,11 @@ public class PowerupSpawner : MonoBehaviour
     [SerializeField] int maxPowerupCount = 10;
     [SerializeField] float minSpawnInterval = 5;
     [SerializeField] float maxSpawnInterval = 25;
-    [SerializeField] List<GameObject> powerupPrefabs;
+
+    [SerializeField] GameObject speedUpPowerup;
+    [SerializeField] GameObject speedDownPowerup;
+    [SerializeField] GameObject fishFrenzyPowerup;
+    [SerializeField] GameObject invertControlsPowerup;
 
     //for spawns
     float xRange = 24;
@@ -29,7 +33,7 @@ public class PowerupSpawner : MonoBehaviour
             Debug.Log(GameManager.gameInstance.isGameActive);
             float spawnInterval = GetSpawnInterval();
             Vector2 spawnLocation = GetSpawnLocation();
-            GameObject powerup = powerupPrefabs[Random.Range(0, powerupPrefabs.Count)];
+            GameObject powerup = PowerupToSpawn();
 
             yield return new WaitForSeconds(spawnInterval);
 
@@ -48,5 +52,31 @@ public class PowerupSpawner : MonoBehaviour
         position.x = Random.Range(-xRange, xRange);
         position.y = yPosition;
         return position;
+    }
+
+    GameObject PowerupToSpawn()
+    {
+        float value = Random.value;
+        GameObject power;
+        //30 % speed up, 30% speed down, 25% invert controls & 15% fish frenzy
+
+        if(value <= 0.15)
+        {
+            power = fishFrenzyPowerup;
+        }
+        else if(value <= 0.4)
+        {
+            power = invertControlsPowerup;
+        }
+        else if(value <= 0.7)
+        {
+            power = speedDownPowerup;
+        }
+        else
+        {
+            power = speedUpPowerup;
+        }
+
+        return power;
     }
 }
